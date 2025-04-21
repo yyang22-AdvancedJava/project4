1146c50ae90f7a470868142b4f10271612ebb50c
Yang <yyang22@madisoncollege.edu>
2025-04-21 12:42:59 -0500

    src/App.js modified

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>My App for Project 4</h1>
+     <h1>My App for Project 4 - Hello Madison, WI</h1>
     </div>
   );
 }
